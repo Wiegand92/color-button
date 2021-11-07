4a8061d56b9d71ffdaa1c586794dad7d55c37861
wiegand92 <wiegand.alex@gmail.com>
2021-11-07 17:57:47 -0500

    setup index.js

---
+++ src/index.js
@@ -0,0 +1,9 @@
+// 3rd Party //
+import React from 'react';
+import reactDOM from 'react-dom';
+
+// Components //
+import App from './components/App';
+
+// Render //
+reactDOM.render(<App />, document.getElementById('app'));

cfe6231fd67b41e547496710acad115ad03a713c
wiegand92 <wiegand.alex@gmail.com>
2021-11-07 19:09:06 -0500

    finish test for button color change

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,7 +1,16 @@
 import React from 'react';
-import {render, screen} from '@testing-library/react';
+import {render, screen, fireEvent} from '@testing-library/react';
 import App from '../components/App';
 
 test('button has correct initial color and text', () => {
-  render(<App/>)
+  render(<App/>);
+  const colorButton = screen.getByRole('button', {name: /change to blue/i});
+  expect(colorButton).toHaveStyle({ backgroundColor: 'red'});
+
+  fireEvent.click(colorButton);
+  expect(colorButton).toHaveStyle({backgroundColor: 'blue'});
+
+  expect(colorButton.textContent).toBe('Change to red');
 });
+
+
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-const App = () => (
-  <h1>Hello World</h1>
-)
+const App = () => {
+  const [buttonColor, setButtonColor] = useState('red');
+  const newButtonColor = buttonColor === 'red' ? 'blue' : 'red';
+
+  return(
+    <div>
+      <button 
+        style={{backgroundColor: buttonColor}}
+        onClick={() => setButtonColor(newButtonColor)}
+      >
+        Change to {newButtonColor}
+      </button>
+    </div>
+  )
+}
 
 export default App;
